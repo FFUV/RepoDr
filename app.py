3024718bbf3dc7a3b36dcfd96ea6a63545c16b1e
Muusy <Ttrmaniacminecraft@gmail.com>
2024-01-22 23:20:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
 @app.route('/search', methods=['POST'])
 def search_projects():
     query = request.form.get('query')
-    response = requests.get(f'https://api.github.com/search/repositories?q={query}+language:python+language:javascript+stars:%3E10')
+    response = requests.get('https://api.github.com/search/repositories?q={}+language:python+language:javascript+stars:%3E10'.format(query))
     data = response.json()
     return jsonify(data['items'])
 
